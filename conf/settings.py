bffeb05cb2b227136bdcc5a0e1eb6f0c97d93c82
ggroshansii <garthgroshansii@gmail.com>
2021-10-29 15:44:07 -0400

    bug troubleshooting: removed forward slash at end of heroku url in allowed host

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['fullstack-gardening-app.herokuapp.com/']
+ALLOWED_HOSTS = ['fullstack-gardening-app.herokuapp.com']
 
 
 # Application definition
