e40a67964eb685738c8d540cb0fb0b3bb219bd8d
ggroshansii <garthgroshansii@gmail.com>
2021-10-30 18:52:20 -0400

    installed react-icons dependency and added copyright in footer with anchor tag of app name directed to github page

--- frontend/static/src/components/Footer/Footer.js
+++ frontend/static/src/components/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { AiOutlineCopyrightCircle } from 'react-icons/ai'
+import './Footer.css'
 
 export default function Footer() {
     return (
-        <div>
-            This is the Footer
+        <div className="footer-container">
+            <p className="footer-copyright"> Copyright <AiOutlineCopyrightCircle /> 2021 <a href="https://github.com/ggroshansii" className="footer-logo-link">Flourish</a>. All Rights Reserved</p>
         </div>
     )
 }
