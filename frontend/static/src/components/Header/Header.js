6428fdfc5b2f40050db84c484b4ee2787bfb9fac
ggroshansii <garthgroshansii@gmail.com>
2021-10-30 22:02:20 -0400

    added free-to-use leaf image for placement by header; added flex to header container

--- frontend/static/src/components/Header/Header.js
+++ frontend/static/src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import Leaf from "../../images/leaf.png";
 
 export default function Header() {
     return (
         <div className="header-container">
-            <h1 className="header-logo">Flourish</h1>
+            <h1 className="header-title">Flourish</h1>
+            <img src={Leaf} alt="green leaf" className="header-leaf" />
         </div>
     )
 }
