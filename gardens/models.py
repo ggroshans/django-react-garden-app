f7dca59784554f9a561fe1820df81fcc758f83cb
ggroshansii <garthgroshansii@gmail.com>
2021-11-01 10:17:59 -0400

    created Garden model within Gardens django app

--- gardens/models.py
+++ gardens/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Garden(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    date = models.DateField(auto_now_add=True)
