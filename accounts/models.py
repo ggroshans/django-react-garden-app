4bc2c091aefe4afacd6ca2a64de8397b79628abe
ggroshansii <garthgroshansii@gmail.com>
2021-11-01 09:48:34 -0400

    added profile model in accounts app with one-to-one relationship with user

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
