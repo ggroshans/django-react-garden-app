a03a28a3b7d0ac5ad582603d2dffbc4ee77bdaf3
ggroshansii <garthgroshansii@gmail.com>
2021-11-01 10:24:38 -0400

    created a listview for the Garden model

--- gardens/views.py
+++ gardens/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Garden
+from .serializers import GardenSerializer
+
 
 # Create your views here.
+
+class GardenListView(generics.ListAPIView):
+    queryset = Garden.objects.all()
+    serializer_class = GardenSerializer
+    
