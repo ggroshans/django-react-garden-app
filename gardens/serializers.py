4e4b4b8c9739ac7e794ff5b7891df72f9c8017d8
ggroshansii <garthgroshansii@gmail.com>
2021-11-01 10:21:50 -0400

    created a serializer python file within Gardens django app and created a GardenSerializer for Garden model

---
+++ gardens/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Garden
+
+class GardenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Garden
+        fields = "__all__"
