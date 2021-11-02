a87d2fe2342b9d12fb64c4d0973417acf66ed465
ggroshansii <garthgroshansii@gmail.com>
2021-11-02 09:59:07 -0400

    added .ReadOnlyField() to return the user's username instead of user id

--- gardens/serializers.py
+++ gardens/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from .models import Garden, Soil
 
 class GardenSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source="user.username")
+
     class Meta:
         model = Garden
-        fields = ('id', 'name', 'user', 'created_at',)
+        fields = ('id', 'name', 'username', 'created_at',)
 
 class SoilSerializer(serializers.ModelSerializer):
     class Meta:
