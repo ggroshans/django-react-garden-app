27a09f1a38da9f24ae45ece9ab4130ea1bdfd562
ggroshansii <garthgroshansii@gmail.com>
2021-11-02 13:50:20 -0400

    added the vegetables form that will be used to query the vegetables table

--- frontend/static/src/components/Main/Vegetables/Vegetables.js
+++ frontend/static/src/components/Main/Vegetables/Vegetables.js
@@ -1,12 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./Vegetables.css";
 
 export default function Vegetables() {
     return (
-        <div>
+        <div className="vegetables-container">
             <h2>Pick Vegetables by Filtering:</h2>
-            <form action="">
-                
+            <form action="" className="form-control vegetables-form">
+                <label htmlFor="sun-exposure">Sun Exposure</label>
+                <select name="sun-exposure" id="">
+                    <option value="both">Both</option>
+                    <option value="full sun">Full Sun</option>
+                    <option value="partial sun">Partial Sun</option>
+                </select>
+
+                <div className="vegetables-checkboxes-container">
+                    <input
+                        type="checkbox"
+                        id="heat-tolerant"
+                        name="heat-tolerant"
+                        value="TRUE"
+                    />
+                    <label htmlFor="heat-tolerant">Heat Tolerant</label>
+                    <input
+                        type="checkbox"
+                        id="drought-tolerant"
+                        name="drought-tolerant"
+                        value="TRUE"
+                    />
+                    <label htmlFor="drought-tolerant">Drought Tolerant</label>
+                </div>
+                <label htmlFor="life-cycle">Life Cycle</label>
+                <select name="life-cycle" id="life-cycle">
+                    <option value="">All</option>
+                    <option value="annual">Annual</option>
+                    <option value="partial sun">Biennial</option>
+                    <option value="partial sun">Perennial</option>
+                </select>
+                <label htmlFor="seasonality">Seasonality</label>
+                <select name="seasonality" id="">
+                    <option value="both">Both</option>
+                    <option value="cool-season">Cool Season</option>
+                    <option value="warm-season">Warm Season</option>
+                </select>
             </form>
         </div>
-    )
+    );
 }
