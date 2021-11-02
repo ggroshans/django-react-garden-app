ac2c6679337a1aa23ac21a4a4ba5685142bf7917
ggroshansii <garthgroshansii@gmail.com>
2021-11-02 12:11:51 -0400

    added setTimeout to map api, which fixed feature service network error

--- frontend/static/src/components/Main/Soil/Soil.js
+++ frontend/static/src/components/Main/Soil/Soil.js
@@ -7,6 +7,7 @@ import { Redirect } from 'react-router-dom'
 
 export default function Soil(props) {
     const firstRender = useRef(true);
+    const [loaded, setLoaded] = useState(false)
     const [soil, setSoil] = useState({
         id: null,
         characteristics: "",
@@ -77,91 +78,103 @@ export default function Soil(props) {
  {
      return <Redirect to="/vegetables" />
  }
+
+ setTimeout(() => {
+     setLoaded(true);
+ })
+
+let esri;
+if (loaded) {
+    esri =             <div className="map-view">
+    <EsriLoaderReact
+        options={options}
+        modulesToLoad={[
+            "esri/config",
+            "esri/Map",
+            "esri/views/MapView",
+            "esri/layers/FeatureLayer",
+            "esri/widgets/Locate",
+            "esri/geometry/Point",
+        ]}
+        onReady={({
+            loadedModules: [
+                esriConfig,
+                Map,
+                MapView,
+                FeatureLayer,
+                Locate,
+                Point,
+            ],
+            containerNode,
+        }) => {
+            esriConfig.apiKey =
+                "AAPKe38b2bd343f04b5193e206b9ba79736dnppkQdQ1hschBAm1A2YPnEoOKzokq3zT2dGou2OnPIINpYr9LR7gfHPGHNy6Lcjy";
+
+            const map = new Map({
+                basemap: "arcgis-topographic",
+            });
+
+            const view = new MapView({
+                container: containerNode,
+                map: map,
+                center: [-118.80543, 34.027],
+                zoom: 13,
+            });
+
+            // const popupSoils = {
+            //     title: "Soil Order",
+            //     content: "<b>{esrisymbology}</b>"
+            // };
+
+            // Parks and open spaces (polygons)
+            const soilsLayer = new FeatureLayer({
+                url: "https://landscape11.arcgis.com/arcgis/rest/services/USA_Soils_Map_Units/featureserver/0",
+                outFields: ["taxorder"],
+                // popupTemplate: popupSoils
+            });
+            map.add(soilsLayer, 0);
+
+            const locate = new Locate({
+                view: view,
+                useHeadingEnabled: false,
+                goToOverride: function (view, options) {
+                    options.target.scale = 1500;
+                    return view.goTo(options.target);
+                },
+            });
+            let soilOrder;
+            view.on("immediate-click", (event) => {
+                const latitude = event.mapPoint.latitude;
+                const longitude = event.mapPoint.longitude;
+                const screenPoint = view.toScreen(
+                    new Point({ latitude, longitude })
+                );
+                view.hitTest(screenPoint).then((hitTestResult) => {
+                    soilOrder =
+                        hitTestResult.results[0].graphic.attributes
+                            .esrisymbology;
+                    setSoil({
+                        id: null,
+                        soil_order: soilOrder,
+                        characteristics: "",
+                        recommendations: "",
+                    });
+                });
+
+            });
+
+            view.ui.add(locate, "top-left");
+        }}
+    />
+</div>
+} else {
+    esri = ""
+}
+
+
     return (
         <div className="soil-container">
-            <div className="map-view" value={soil}>
-
-                <EsriLoaderReact
-                    options={options}
-                    modulesToLoad={[
-                        "esri/config",
-                        "esri/Map",
-                        "esri/views/MapView",
-                        "esri/layers/FeatureLayer",
-                        "esri/widgets/Locate",
-                        "esri/geometry/Point",
-                    ]}
-                    onReady={({
-                        loadedModules: [
-                            esriConfig,
-                            Map,
-                            MapView,
-                            FeatureLayer,
-                            Locate,
-                            Point,
-                        ],
-                        containerNode,
-                    }) => {
-                        esriConfig.apiKey =
-                            "AAPKe38b2bd343f04b5193e206b9ba79736dnppkQdQ1hschBAm1A2YPnEoOKzokq3zT2dGou2OnPIINpYr9LR7gfHPGHNy6Lcjy";
-
-                        const map = new Map({
-                            basemap: "arcgis-topographic",
-                        });
-
-                        const view = new MapView({
-                            container: containerNode,
-                            map: map,
-                            center: [-118.80543, 34.027],
-                            zoom: 13,
-                        });
-
-                        // const popupSoils = {
-                        //     title: "Soil Order",
-                        //     content: "<b>{esrisymbology}</b>"
-                        // };
-
-                        // Parks and open spaces (polygons)
-                        const soilsLayer = new FeatureLayer({
-                            url: "https://landscape11.arcgis.com/arcgis/rest/services/USA_Soils_Map_Units/featureserver/0",
-                            outFields: ["taxorder"],
-                            // popupTemplate: popupSoils
-                        });
-                        map.add(soilsLayer, 0);
-
-                        const locate = new Locate({
-                            view: view,
-                            useHeadingEnabled: false,
-                            goToOverride: function (view, options) {
-                                options.target.scale = 1500;
-                                return view.goTo(options.target);
-                            },
-                        });
-                        let soilOrder;
-                        view.on("immediate-click", (event) => {
-                            const latitude = event.mapPoint.latitude;
-                            const longitude = event.mapPoint.longitude;
-                            const screenPoint = view.toScreen(
-                                new Point({ latitude, longitude })
-                            );
-                            view.hitTest(screenPoint).then((hitTestResult) => {
-                                soilOrder =
-                                    hitTestResult.results[0].graphic.attributes
-                                        .esrisymbology;
-                                setSoil({
-                                    id: null,
-                                    soil_order: soilOrder,
-                                    characteristics: "",
-                                    recommendations: "",
-                                });
-                            });
-
-                        });
-
-                        view.ui.add(locate, "top-left");
-                    }}
-                />
-            </div>
+            {esri}
             <div className="display-soil-container">
                 <p className="display-soil-p">{soil.soil_order}</p>
             </div>
