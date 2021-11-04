5dcd371fbec78d7c0aa2aa779366b20bb9f3d739
ggroshansii <garthgroshansii@gmail.com>
2021-11-04 09:14:05 -0400

    created onClick function that will eventually send user to a detailed view of that specific garden

--- frontend/static/src/components/Main/Profile/GardenItem.js
+++ frontend/static/src/components/Main/Profile/GardenItem.js
@@ -3,12 +3,16 @@ import "./GardenItem.css"
 
 export default function GardenItem(props) {
 
+    function handleGardenItemClick() {
+        
+    }
+
     console.log("g-item props", props)
 
     return (
         <div className='garden-item-container'>
        
-            <div className="garden-item">
+            <div className="garden-item" onClick={handleGardenItemClick}>
                 <h3>{props.name}</h3>
                 <p>Created: {props.created_at}</p>
                 <p>Click for more details</p>
