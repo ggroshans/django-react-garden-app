24e4f764062d6e69337e5ac68f821dbd3d774c43
ggroshansii <garthgroshansii@gmail.com>
2021-11-06 20:44:24 -0400

    placed absolute positioning on navbar and centered it

--- frontend/static/src/components/Header/Header.js
+++ frontend/static/src/components/Header/Header.js
@@ -5,7 +5,7 @@ import { Redirect, withRouter } from "react-router-dom";
 import Cookie from "js-cookie";
 import { useEffect, useState } from "react";
 import Farmer from "../../images/farmer.png";
-import {Button, Collapse} from 'react-bootstrap';
+import { Button, Collapse, Spinner } from "react-bootstrap";
 
 function Header(props) {
     const [username, setUsername] = useState("");
@@ -33,43 +33,78 @@ function Header(props) {
         }
     }
 
-    // function handleClick() {
-    //     if (props.isAuth) {
-    //         props.history.push(`/gardenlist/`);
-    //     } else {
-    //         props.history.push(`/`);
-    //     }
-    // }
-
-    // function handleUserIconClick() {
-    //     props.history.push("/gardenlist/");
-    // }
-
     function handleProfileClick() {
         props.history.push("/gardenlist/");
         setOpen(false);
     }
 
-  async function handleLogoutClick() {
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "X-CSRFToken": Cookie.get("csrftoken"),
-        },
-    };
-    const response = await fetch("/rest-auth/logout/", options);
-    if (response.ok === false) {
-        console.log("LOGOUT FAILED", response);
-    } else {
-        const data = await response.json();
-        Cookie.remove("Authorization")
-        console.log("LOGOUT SUCCESSFUL", data)
-        props.history.push("/");
-        props.setIsAuth(false);
-        setOpen(false);
+    async function handleLogoutClick() {
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRFToken": Cookie.get("csrftoken"),
+            },
+        };
+        const response = await fetch("/rest-auth/logout/", options);
+        if (response.ok === false) {
+            console.log("LOGOUT FAILED", response);
+        } else {
+            const data = await response.json();
+            Cookie.remove("Authorization");
+            console.log("LOGOUT SUCCESSFUL", data);
+            props.history.push("/");
+            props.setIsAuth(false);
+            setOpen(false);
+            setUsername("");
+        }
     }
-}
+
+    let html;
+    if (props.isAuth && username) {
+        html = (
+            <>
+                {" "}
+                <div
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                >
+                    <div className="header-user-container">
+                        <p className="header-username">{username}</p>
+                        <div className="header-user-icon-container">
+                            {" "}
+                            <img
+                                src={Farmer}
+                                alt=""
+                                className="header-user-icon"
+                            />{" "}
+                        </div>
+                    </div>
+                </div>
+                <Collapse in={open}>
+                    <div className="header-user-collapse">
+                        <button
+                            className="header-user-profile-btn"
+                            onClick={handleProfileClick}
+                        >
+                            Profile
+                        </button>
+                        <button
+                            className="header-user-logout-btn"
+                            onClick={handleLogoutClick}
+                        >
+                            Logout
+                        </button>
+                    </div>
+                </Collapse>{" "}
+            </>
+        );
+    } else {
+        return (
+            ""
+        )
+    }
 
     return (
         <div className="header-container">
@@ -77,31 +112,7 @@ function Header(props) {
                 <h1 className="header-title">Flourish</h1>
                 <img src={Leaf} alt="green leaf" className="header-leaf" />
             </div>
-            <div
-                onClick={() => setOpen(!open)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open}
-            >
-                <div
-                    className="header-user-container"
-                >
-                    <p className="header-username">{username}</p>
-                    <div className="header-user-icon-container">
-                        {" "}
-                        <img
-                            src={Farmer}
-                            alt=""
-                            className="header-user-icon"
-                        />{" "}
-                    </div>
-                </div>
-            </div>
-            <Collapse in={open}>
-                <div className="header-user-collapse">
-                    <button className="header-user-profile-btn" onClick={handleProfileClick}>Profile</button>
-                    <button className="header-user-logout-btn" onClick={handleLogoutClick}>Logout</button>
-                </div>
-            </Collapse>
+            {html}
         </div>
     );
 }
