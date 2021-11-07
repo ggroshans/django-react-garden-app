fbee855031c41d33ec9246566d04a247306b5547
ggroshansii <garthgroshansii@gmail.com>
2021-11-07 14:11:57 -0500

    styled the UserVegetableList component

--- frontend/static/src/components/Main/Vegetables/UserVegetableList.js
+++ frontend/static/src/components/Main/Vegetables/UserVegetableList.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 export default function UserVegetableList(props) {
     return (
         <div className="user-vegetable-list-container">
-        <h2>Your Vegetables:</h2>
+        <h2 className="user-vegetable-heading">Your Vegetables:</h2>
             <div className="user-vegetables-grid-container">
             {props.userVegetables === undefined
                 ? ""
@@ -14,7 +14,7 @@ export default function UserVegetableList(props) {
                           {console.log("VEGGIE",vegetable)}
                           <div className="user-vegetable" key={uuidv4()}>
                               <p className="user-vegetable-name">{vegetable.name}</p>
-                              <button className="user-add-vegetable" onClick={() => props.removeFromUserList(vegetable.id)}>Remove</button>
+                              <button className="btn btn-success user-add-vegetable-btn" onClick={() => props.removeFromUserList(vegetable.id)}>Remove</button>
                           </div></>
                       );
                   })}
