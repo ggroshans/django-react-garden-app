be87e31268496df5d07ae514a13e2d85ecb0e4cd
ggroshansii <garthgroshansii@gmail.com>
2021-11-09 21:04:44 -0500

    overhaul in styling splash page

--- frontend/static/src/components/Auth/Splash.js
+++ frontend/static/src/components/Auth/Splash.js
@@ -1,21 +1,20 @@
 import React from "react";
 import "./Splash.css";
 import { NavLink, Redirect } from "react-router-dom";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function Splash(props) {
-
-    useEffect( ()=> {
+    useEffect(() => {
         props.setShowHeader(false);
-    }, [])
+    }, []);
 
     if (props.isAuth) {
-       return <Redirect to="/gardenlist" />
+        return <Redirect to="/gardenlist" />;
     }
 
     return (
         <div className="splash-container">
-            <h1 className="splash-main-heading">
+            {/* <h1 className="splash-main-heading">
                 Welcome to <span className="flourish-span">Flourish</span>
             </h1>
             <h2 className="splash-sub-heading">
@@ -30,28 +29,7 @@ export default function Splash(props) {
                 important gardening areas: <span className="splash-soil">Soil</span>, <span className="splash-vegetables">Vegetables</span>, <span className="splash-companion">Companion Planting</span>,
                 and  <span className="splash-layout">Physical Layout</span>.
             </p>
-            {/* <ol className="splash-list">
-                <li className="splash-list-step">
-                    {" "}
-                    <span className="splash-soil splash-lobster-font">1) Soil:</span> Find your soil type based on your desired location.
-                    Using your specific soil type, we give your soil
-                    characteristics and recommendtations for potential
-                    improvements.
-                </li>
-                <li className="splash-list-step">
-                    {" "}
-                    <span className="splash-vegetables splash-lobster-font">2) Vegetables:</span> Considering the varying conditions of your garden, we allow you to filter against plant characteristics so you can choose the most appropriate vegetables specific areas of your garden.
-                </li>
-                <li className="splash-list-step">
-                    {" "}
-                    <span className="splash-companion splash-lobster-font">3) Companion Planting:</span> Vegetables often do better if they are planted alongside other plants; however, there can also be plants you do not want to plant together. We help find the best companion plants for your chosen vegetables. 
-                </li>
-                <li className="splash-list-step">
-                    {" "}
-                    <span className="splash-layout splash-lobster-font"> 4) Physical Layout:</span> Drafting the visual layout of your garden can be highly beneficial for garden preparation. We allow you to sketch an outline of your garden beds, while providing an area for you to jot down important notes.
-                </li>
 
-            </ol> */}
             <div className="splash-btn-group">
             <NavLink to="/login">
                 <button
@@ -69,6 +47,48 @@ export default function Splash(props) {
                     Register
                 </button>
                 </NavLink>
+            </div> */}
+
+            <div className="splash1 position-relative w-100">
+                <div className="splash2 position-absolute text-white d-flex flex-column align-items-start justify-content-center">
+                    <div className="container">
+                        <div class="col-md-6">
+                            {/* <span className="splash-subheading text-uppercase">
+                                Plan, organize, cultivate
+                            </span> */}
+                            <h1 className="splash-name mb-4 mt-2 display-4 font-weight-bold">
+                                Welcome to
+                                <span className="splash-headline">
+                                    Flourish
+                                </span>
+                            </h1>
+                            <p className="splash-p">
+                                Flourish is a tool to help you prepare your
+                                garden for the upcoming season, while allowing
+                                you to cut down the time it takes to lay the
+                                groundwork.
+                            </p>
+                            <div className="mt-5">
+                            <NavLink to="/registration">
+                                <a
+                                    href="#"
+                                    className="splash-btns btn px-5 py-3 text-white mt-3 mx-3 mt-sm-0"
+                                >
+                                    Register
+                                </a>
+                                </NavLink>
+                                <NavLink to="/login">
+                                <a
+                                    href="#"
+                                    className="splash-btns btn px-5 py-3 text-white mt-3 mt-sm-0"
+                                >
+                                    Login
+                                </a>
+                                </NavLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
